deedf44fd726c27f6f5b5009730a5a4465c71c9f
zer0mana <balabanovmm@gmail.com>
2023-05-26 14:01:26 +0300

    Add new migrations

--- OrderHandlerMicroservice/Program.cs
+++ OrderHandlerMicroservice/Program.cs
@@ -33,4 +33,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MigrateUp();
+
 app.Run();
---
+++ OrderHandlerMicroservice/Repositories/Migrations/005_AddOrderDishType.cs
@@ -0,0 +1,41 @@
+﻿using FluentMigrator;
+
+namespace OrderHandlerMicroservice.Repositories.Migrations;
+
+[Migration(005, TransactionBehavior.None)]
+public class AddOrderDishType : Migration {
+    public override void Up()
+    {
+        const string sql = @"
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'calculations_v1') THEN
+            CREATE TYPE calculations_v1 as
+            (
+                  id           bigint
+                , user_id      bigint
+                , good_ids     bigint[]
+                , total_volume double precision
+                , total_weight double precision
+                , price        numeric(19, 5)
+                , at           timestamp with time zone
+            );
+        END IF;
+    END
+$$;";
+        
+        Execute.Sql(sql);
+    }
+
+    public override void Down()
+    {
+        const string sql = @"
+DO $$
+    BEGIN
+        DROP TYPE IF EXISTS calculations_v1;
+    END
+$$;";
+
+        Execute.Sql(sql);
+    }
+}
